5009f259531e76c871fbb32fadf363b4ce1f329b
Gaurav Lagad <gauravlagad2001@gmail.com>
2023-03-23 17:18:35 +0530

    after_adding_j2

---
+++ j2.java
@@ -0,0 +1,9 @@
+public class j2{
+    public static void main(String args[]){
+        int a = 17;
+        int b=187;
+        int c= a-b;
+        System.out.println(c);
+        System.out.println("program end");
+    }
+}
